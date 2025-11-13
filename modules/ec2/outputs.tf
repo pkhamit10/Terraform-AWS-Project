079086a73b3b743170e5619195e9ed702d23a9c4
PK <pk@PKs-MacBook-Pro.local>
2025-11-13 17:42:46 +0000

    Revert "created modules and added the resources accordingly"
    This reverts commit dbcdcdd1f9b44edd246b750d3b438e108d214474.

--- modules/ec2/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "web_instance_public_ip" {
-  value = aws_instance.servers["web_server"].public_ip
-}
-
-output "db_instance_private_ip" {
-  value = aws_instance.servers["db_server"].private_ip
-}
