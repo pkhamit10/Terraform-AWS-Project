9180b8064b00112e4031c3c10f604c2bc8a7f49f
PK <pk@PKs-MacBook-Pro.local>
2025-11-13 19:16:54 +0000

    added modules and resources to networking

---
+++ modules/networking/main.tf
@@ -0,0 +1,71 @@
+#create vpc for the infrastructure
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+#setup subnets using for_each to iterate over the map variable  
+resource "aws_subnet" "subnets" {
+  for_each                = var.subnets
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.availability_zone
+  map_public_ip_on_launch = each.value.map_public_ip_on_launch
+
+  tags = {
+    Name = "${each.value.cidr_block}-${each.value.availability_zone}"
+  }
+}
+
+#Attach an Internet Gateway (IGW) to the VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id       
+    tags = {
+        Name = "${var.vpc_name}-igw"
+    }
+}
+
+#Create a route table for the VPC and add a route to the IGW       
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block =  "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+#Associate the public route table with the public subnets
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.subnets["subnet-a"].id
+  route_table_id = aws_route_table.public_rt.id
+}   
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.subnets["subnet-a"].id
+  tags = {
+    Name = "nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.igw] 
+}
+
+# -------------------------
+# Allocate Elastic IP for NAT Gateway
+# -------------------------
+resource "aws_eip" "nat_eip" {
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_route" "nat_route" {
+  route_table_id         = aws_vpc.this.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
