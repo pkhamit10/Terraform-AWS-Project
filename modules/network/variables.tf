dbcdcdd1f9b44edd246b750d3b438e108d214474
PK <pk@PKs-MacBook-Pro.local>
2025-11-13 17:20:24 +0000

    created modules and added the resources accordingly

---
+++ modules/network/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "subnets" {
+  type = map(object({
+    cidr_block              = string
+    availability_zone       = string
+    map_public_ip_on_launch = bool
+  }))
+}
